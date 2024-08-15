94d2c4e4896daa90822b5ab602dec14c57c9509d
Nusratullokh <nusratullokh7700@gmail.com>
2024-08-15 08:13:08 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let user = {
+    name: "John",
+    age: 30,
+    email: "Xgk7w@example.com",
+    addres: "123 Main St"
+};
---
+++ ts/index.ts
@@ -0,0 +1,14 @@
+type User ={
+    name: string
+    age: number
+    email: string
+    addres: string | number
+
+}
+let user: User ={
+    name: "John",
+    age: 30,
+    email: "Xgk7w@example.com",
+    addres: "123 Main St"
+
+}
